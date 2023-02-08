31736dcce893fdc625b0f3f46f73986926b04c4b
ezrablackfish2 <105113257+ezrablackfish2@users.noreply.github.com>
2023-02-08 09:28:18 +0300

    Time: 121 ms (61.99%), Space: 14.6 MB (23.08%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        prefix_sum = [0]*len(nums)
+        prefix_sum[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i]
+            
+    
+        prefix_sum.insert(0, 0)
+        prefix_sum.append(0)
+        
+        n = len(prefix_sum)
+        tot = sum(nums)
+        for i in range(1, n-1):
+            if prefix_sum[i-1] == tot - prefix_sum[i]:
+                return i-1
+        return -1
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
