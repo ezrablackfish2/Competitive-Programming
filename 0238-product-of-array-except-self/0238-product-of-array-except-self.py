929360b484970bce62e241626d4bb6c06ea07d9c
ezrablackfish2 <105113257+ezrablackfish2@users.noreply.github.com>
2023-02-08 09:36:22 +0300

    Time: 181 ms (88.65%), Space: 21.1 MB (41.57%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        pref_prod = 1
+        ans = []
+        
+        for num in nums:
+            ans.append(pref_prod)
+            pref_prod *= num
+	
+        # answer = leftProduct * rightProduct
+        # here we are using reversed nums and ans
+        suf_prod = 1
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= suf_prod
+            suf_prod *= nums[i]
+        
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
